ba8dce9ce9f8a53b670a3a8322d2cc36279b3f9c
saket <saket@LAPTOP-QTO42I3L>
2022-06-18 18:26:36 +0530

    first commit

---
+++ Day_12/src/problem1.java
@@ -0,0 +1,12 @@
+
+public class problem1 {
+	public static void main(String args[]){
+		String str = "Programming";
+		String s = new String("World");
+		System.out.println(str);
+		System.out.println(s);
+		}
+
+}
+
+
